1b6cd6129e74689c56bb9721165fb45079d7f0c8
niklassc <niklas@schmatloch.de>
2021-05-17 16:56:40 +0200

    add round model

---
+++ src/components/shared/models/Round.js
@@ -0,0 +1,21 @@
+/**
+ * Round Model
+ */
+
+class Game{
+  status;
+  constructor(data= {}) {
+    this.drawerName = data.drawerName;
+    this.gotPoints = data.getPoints;
+    this.hasDrawn = data.hasDrawn;
+    this.hasGuessed = data.hasGuessed;
+    this.id = data.id;
+    this.index = data.index;
+    this.players = data.players;
+    this.status = data.status;
+    this.word = data.word;
+    this.words = data.words;
+  }
+}
+
+export default Round;
