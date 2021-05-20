09af7fd3c09bc6e2759e52600d5878e93b83f37a
JosipHarambasic <harambasic.josip97@gmail.com>
2021-05-20 20:30:09 +0200

    imported stuff

--- src/components/waitingScreen/WaitingScreen.js
+++ src/components/waitingScreen/WaitingScreen.js
@@ -13,6 +13,9 @@ import { Legend } from '../../views/design/Legend.js';
 import { InputField } from '../../views/design/InputField.js';
 import { OneLineBlock } from '../../views/design/OneLineBlock.js';
 import { SelectField } from '../../views/design/SelectField.js';
+import Message from "../../views/Message";
+import {Chatbox} from "../../views/design/Chatbox";
+import {Messages} from "../../views/design/Messages";
 
 
 const PlayerUl = styled.ul`
